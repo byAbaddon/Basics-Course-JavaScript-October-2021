6b84ed9825da0bcf7923b2e1b98c8240a663f3bb
byAbaddon <51271834+byAbaddon@users.noreply.github.com>
2023-02-08 15:45:18 +0200

    Update 04-walking.js

--- 5.1 While Loop - Exercise/04-walking.js
+++ 5.1 While Loop - Exercise/04-walking.js
@@ -1,3 +1,9 @@
+arg => {
+  allStp = eval(arg.filter(Number).join('+')) - 10_000
+  return allStp < 0 ? `${Math.abs(allStp)} more steps to reach goal.`: `Goal reached! Good job!\n${allStp} steps over the goal!`
+}
+
+//--------------------------------------------(2)-------------------------------------
 function walking(arg) {
   let steps = 10000
   let allSteps = arg.filter(el => Number(el)).reduce((a, b) => +a + +b) - steps
